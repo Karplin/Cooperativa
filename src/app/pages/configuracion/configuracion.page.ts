8ba8d5b276f163860c309b08c1d521538c4de196
Leonal Perez <Karplincontact@gmail.com>
2022-04-23 22:48:21 -0400

    Configuración - Tasas
    - Implementación de la vista "Configuración".
    - Implementación de la vista "Tasas":

--- src/app/pages/configuracion/configuracion.page.ts
+++ src/app/pages/configuracion/configuracion.page.ts
@@ -5,11 +5,49 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './configuracion.page.html',
   styleUrls: ['./configuracion.page.scss'],
 })
-export class ConfiguracionPage implements OnInit {
+export class ConfiguracionPage {
+
+  showPassword  = false;
+  showPassword1 = false;
+  showPassword2 = false;
+
+
+  passwordToggleIcon  = 'eye';
+  passwordToggleIcon2  = 'eye';
+  passwordToggleIcon3  = 'eye';
 
   constructor() { }
 
-  ngOnInit() {
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  
+    if (this.passwordToggleIcon === 'eye' ) {
+      this.passwordToggleIcon = 'eye-off';
+    } else {
+      this.passwordToggleIcon = 'eye';
+    }
   }
+
+  togglePassword2(): void {
+    this.showPassword1 = !this.showPassword1;
+
+
+    if (this.passwordToggleIcon2 === 'eye' ) {
+      this.passwordToggleIcon2 = 'eye-off';
+    } else {
+      this.passwordToggleIcon2 = 'eye';
+    }
+  }
+
+  togglePassword3(): void {
+    this.showPassword2 = !this.showPassword2;
+
+    if (this.passwordToggleIcon3 === 'eye' ) {
+      this.passwordToggleIcon3 = 'eye-off';
+    } else {
+      this.passwordToggleIcon3 = 'eye';
+    }
+  }
+ 
 
 }
